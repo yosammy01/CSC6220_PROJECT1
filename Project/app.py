40685b8a03130e967f2488da3d982b3edf40f276
yosammy01 <samueleverardorobles@gmail.com>
2026-02-14 12:21:22 -0500

    Initial commit

---
+++ Project/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# home page route/location
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# about page route/location
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# networking concepts page route/location
+@app.route('/networking')
+def networking():
+    return render_template('networking.html')
+
+#run the app in debug mode
+if __name__ == '__main__':
+    app.run(debug=True)
