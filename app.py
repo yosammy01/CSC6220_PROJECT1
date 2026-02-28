52cb9287b056c26546c2a412be5ead8654c2a79e
yosammy01 <samueleverardorobles@gmail.com>
2026-02-28 09:43:47 -0500

    final changes to host on github pages
    I needed to change over from flask/python to javascript style website. I left the flask/python structure in place while incorporating the new javascript style.

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask, render_template, request, redirect, send_from_directory, url_for
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 # home page route/location
 @app.route('/')
 def index():
-    # send_from_directory looks in the specified folder ('.' means the current root directory)
-    # and serves the 'index.html' file found there.
-    return send_from_directory('.', 'index.html')
+    return render_template('index.html')
 
 # about page route/location
 @app.route('/about')
